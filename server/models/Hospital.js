f438f2b788f38bcaf071805832e88645db9992eb
Sai <saisandhya187@gmail.com>
2020-11-07 10:47:20 +0530

    Password for hospital reg

--- server/models/Hospital.js
+++ server/models/Hospital.js
@@ -1,14 +1,15 @@
 const { Schema, model } = require('mongoose');
 
 const HospitalSchema = new Schema({
-    name:{
-        type:String,
-        trim:true,
-        required:true
+    name: {
+        type: String,
+        trim: true,
+        required: true
     },
     city: String,
     State: String,
     contactNo: Number,
+    password: String,
     plasmaFacility: Boolean,
     bedAvailable: Number
 })
--- server/routes/hospitals/register.js
+++ server/routes/hospitals/register.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt')
 const register = require('express').Router();
 const Hospital = require('../../models/Hospital')
 const { createJWTtoken } = require('../../middlewares/jwt');
@@ -6,13 +7,14 @@ const { createJWTtoken } = require('../../middlewares/jwt');
 
 register.post('/', async (req, res) => {
     try {
-        let { name, city, state, contactNo } = req.body
-        if (!name || !city || !state || !contactNo) {
+        let { name, city, state, contactNo, password } = req.body
+        if (!name || !city || !state || !contactNo || !password) {
             return res.status(400).json({ message: "Fill all fields" })
         }
         const hospital = await Hospital.findOne({ contactNo })
         if (hospital == null) {
-            const newHospital = new Hospital(req.body)
+            const hashedPassword = await bcrypt.hash(req.body.password, 10);
+            const newHospital = new Hospital({ name, city, state, contactNo, password: hashedPassword })
             await newHospital.save()
             const token = await createJWTtoken(newHospital)
             res.status(200).json({
@@ -29,6 +31,5 @@ register.post('/', async (req, res) => {
     }
 })
 
-//login
 
 module.exports = register;
