97a6dfc6cdf28b1772f9c32f9fc449ba32040e4c
Sai <saisandhya187@gmail.com>
2020-11-07 08:00:22 +0530

    Added .env.example file

--- server/index.js
+++ server/index.js
@@ -30,5 +30,5 @@ app.use('/auth/plasmaDonor', require('./routes/plasmaDonor/user'));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`server running on port:${PORT}`)
+    console.log(`Server running on port: ${PORT}`)
 });
